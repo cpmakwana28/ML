1f19fb4cc9da9454e226240ea2e57323359c5e30
me-28 <38167198+me-28@users.noreply.github.com>
2018-06-28 00:41:58 +0530

    Add files via upload

---
+++ speech_recognition.py
@@ -0,0 +1,10 @@
+# NOTE: this example requires PyAudio because it uses the Microphone class
+
+import speech_recognition as sr
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+print(r.recognize_google(audio))
